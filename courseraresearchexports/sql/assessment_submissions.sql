a8ab848ca4c9a3d29dc26a4bb6061f3a5cce620c
Chris Liu <cliu@coursera.org>
2016-09-14 15:31:24 -0700

    Add example assessment denormalizations

---
+++ courseraresearchexports/sql/assessment_submissions.sql
@@ -0,0 +1,36 @@
+/*
+Returns 1 row per assessment submission that is bridged to the course item
+corresponding to that particular assessment.
+*/
+WITH action_ids_with_responses AS
+(
+    SELECT DISTINCT course_branch_id
+        ,course_item_id
+        ,assessment_action_id
+    FROM assessment_responses
+    JOIN course_branch_item_assessments
+        USING(assessment_id)
+)
+
+SELECT course_id
+		,course_branch_id
+    ,course_item_id
+    ,assessment_id
+    ,assessment_action_id
+    ,[partner_short_name]_user_id
+    ,assessment_action_ts
+    ,assessment_action_ts = MIN(assessment_action_ts) OVER(
+        PARTITION BY assessment_id, [partner_short_name]_user_id
+    ) AS is_first_submission
+    ,assessment_action_ts = MAX(assessment_action_ts) OVER(
+        PARTITION BY assessment_id, [partner_short_name]_user_id
+    ) AS is_last_submission
+    ,assessment_action_ts = MIN(assessment_action_ts) OVER(
+        PARTITION BY course_id, course_item_id, [partner_short_name]_user_id
+    ) AS is_first_item_submission
+FROM assessment_actions
+JOIN action_ids_with_responses
+    USING(assessment_action_id)
+JOIN course_branches
+		USING(course_branch_id)
+;
