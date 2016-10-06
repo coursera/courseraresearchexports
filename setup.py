065c79d414fe2816168e622f56fdc4a96c905008
Timothy Lee <tlee@coursera.org>
2016-10-06 13:21:44 -0700

    Bump version for release

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='courseraresearchexports',
-    version='0.0.14',
+    version='0.0.15',
     description='Command line tool for convenient access to '
     'Coursera Research Data Exports.',
     long_description=readme(),
