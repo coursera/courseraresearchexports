5a1e54a54c5c261955c8c29da84e42ca5d0806c0
Timothy Lee <tlee@coursera.org>
2017-02-24 11:51:38 -0800

    Use partners API when inferring id from short name (#46)
    * Use partners API when inferring id from short name
    
    * Bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='courseraresearchexports',
-    version='0.0.19',
+    version='0.0.20',
     description='Command line tool for convenient access to '
     'Coursera Research Data Exports.',
     long_description=readme(),
