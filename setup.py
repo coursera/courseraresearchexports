2ce48e94da4d001c5c7484797181751ffee46e72
Amory Schlender <aschlender@coursera.org>
2017-03-28 15:40:17 -0700

    Adds executable code blocks to research exports (#50)
    * Adds executable code blocks to research exports
    
    * bump version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ def readme():
 
 setup(
     name='courseraresearchexports',
-    version='0.0.22',
+    version='0.0.23',
     description='Command line tool for convenient access to '
     'Coursera Research Data Exports.',
     long_description=readme(),
